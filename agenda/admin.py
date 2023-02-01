dac75094ec07816cb22c4553466e643b52fa9fed
carlos roberto <carlosrobertounopar01@hotmail.com>
2023-02-01 01:53:08 -0300

    monstando classes na tela do admin

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from agenda.models import Evento, Categoria
 
-# Register your models here.
+#Registra as classe para imprimir na tela admin.
+admin.site.register(Evento)
+admin.site.register(Categoria)
