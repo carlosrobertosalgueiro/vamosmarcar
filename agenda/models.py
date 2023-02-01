c8d541a81b8763dc9716b26c0a5b511b5c6900b4
carlos roberto <carlosrobertounopar01@hotmail.com>
2023-02-01 01:55:11 -0300

    inserido função str que mostra o nome real dos objetos na tela

--- agenda/models.py
+++ agenda/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Evento:
-        def __init__(self, nome, categoria, local=None, link=None):
-             self.nome = nome
-             self.categoria = categoria
-             self.local = local
-             self.link = link
 
-aula_python = Evento("Aula de python", "backend", "Salgueiro") 
-aula_java = Evento("Aula de java", "backend","petrolina", link="salgueiro.com")   
+class Categoria(models.Model):
+    nome = models.CharField(max_length=265, unique=True)
 
-eventos = [
-    aula_python, aula_java
-]
+#aimprime observações do objeto
+    def __str__(self):
+        return f"{self.nome} <{self.id}>"
+
+class Evento(models.Model):
+    nome = models.CharField(max_length=256)
+    Categoria = models.ForeignKey(
+        Categoria, on_delete=models.SET_NULL, null=True)
+    local = models.CharField(max_length=256, blank=True)
+    link = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.Categoria} - {self.local} - {self.link}"
