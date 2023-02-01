f7b11164a1f570c3255e713d1a6658d5edb1efb4
carlos roberto <carlosrobertounopar01@hotmail.com>
2023-02-01 02:00:35 -0300

    inserido a função que lista os evento e envia para o html

--- agenda/views.py
+++ agenda/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from agenda.models import eventos
 from django.template import loader
+from agenda.models import Evento
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Olá, mundo!")
+
+def listar_eventos(request):
+    eventos = Evento.objects.all()
+    return render(request=request, context={"eventos": eventos}, template_name="agenda/listar_evento.HTML")
+
+
 
 def exibir_evento(request):
-    evento =  eventos[1]  
-    return render(request=request, context={"evento": evento},template_name="agenda/exibir_agenda.HTML")
-
-    
+    evento = {"nome": "Aula de java",
+              "categoria": "backend",
+              "local": "petrolina",
+              "link": "salgueiro.com"}
+    return render(request=request, context={"evento": evento}, template_name="agenda/exibir_agenda.HTML")
