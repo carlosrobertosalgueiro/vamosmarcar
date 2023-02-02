7617f5e055147509c22ac16f2d72288ad89914b9
carlos roberto <carlosrobertounopar01@hotmail.com>
2023-02-02 01:06:27 -0300

    inserido data

--- agenda/models.py
+++ agenda/models.py
@@ -6,9 +6,10 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=265, unique=True)
 
-#aimprime observações do objeto
+# aimprime observações do objeto
     def __str__(self):
         return f"{self.nome} <{self.id}>"
+
 
 class Evento(models.Model):
     nome = models.CharField(max_length=256)
@@ -16,6 +17,7 @@ class Evento(models.Model):
         Categoria, on_delete=models.SET_NULL, null=True)
     local = models.CharField(max_length=256, blank=True)
     link = models.CharField(max_length=256, blank=True)
+    data = models.DateField(null=True)
 
     def __str__(self):
         return f"{self.nome} - {self.Categoria} - {self.local} - {self.link}"
